04bb409d1779b2766d1ec8ec1ae68df8eb762402
pedromrtns <pcruz6064@gmail.com>
2024-06-04 23:34:02 -0300

    segundo commit

--- setup/settings.py
+++ setup/settings.py
@@ -123,12 +123,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#configuração de email
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.office365.com' 
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  
-
